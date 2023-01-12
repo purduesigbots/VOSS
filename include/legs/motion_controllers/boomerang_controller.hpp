03e94d8cfe7e6d701ddca05ae061642bebe37273
Andrew Lu <lu987@purdue.edu>
2023-01-12 17:32:07 -0500

    add more stuff to header files

--- include/legs/motion_controllers/boomerang_controller.hpp
+++ include/legs/motion_controllers/boomerang_controller.hpp
@@ -2,6 +2,7 @@
 
 #include "api.h"
 #include "legs/motion_controllers/basic_controller.hpp"
+#include "legs/pid.hpp"
 
 namespace legs {
 
@@ -10,13 +11,48 @@ class BoomerangController;
 
 class BoomerangController : public BasicController
 {
-    void move(Eigen::Vector3d& target);
-    void turn(double target);
+    #define DISABLE 0
+    #define TRANSLATIONAL 1
+    #define ANGULAR 2
+    
+    public:
+        void move(Eigen::Vector3d& target);
+        void turn(double target);
+    
+    protected:
+        BoomerangController();
+    
+    private:
+        int mode = DISABLE;
+        Pid linearPid;
+        Pid angularPid;
+        double linear_exit_error;
+        double angular_exit_error;
+        double linear_settle_thresh;
+        double angular_settle_thresh;
+        double settle_time;
+        double max_speed;
+        double lead_pct;
+        bool reverse;
+        bool thru;
+        bool can_reverse;
 };
 
 class BoomerangControllerBuilder
 {
+    public:
+        BoomerangControllerBuilder& withLinearPid(double p, double i, double d);
+        BoomerangControllerBuilder& withAngularPid(double p, double i, double d);
+        BoomerangControllerBuilder& withExitErrors(double linear, double angular);
+        BoomerangControllerBuilder& withSettleThresh(double linear, double angular);
+        BoomerangControllerBuilder& withSettleTime(double time);
 
+        BoomerangController build();
+
+        BoomerangControllerBuilder();
+    
+    private:
+        BoomerangController controller;
 };
 
 }
--- include/legs/motion_models/motion_model.hpp
+++ include/legs/motion_models/motion_model.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
-#include "legs/motion_model/basic_model.hpp"
-#include "legs/motion_model/odometry.hpp"
+#include "legs/motion_models/basic_model.hpp"
+#include "legs/motion_models/odometry.hpp"
