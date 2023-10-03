d1a30eecf64ceb01cf48d84b8375b0450d98444f
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-10-03 19:00:29 -0400

    Remove atomic
    Builds, will readd atomic later

--- include/VOSS/localizer/ADILocalizer.hpp
+++ include/VOSS/localizer/ADILocalizer.hpp
@@ -2,35 +2,34 @@
 
 #include "pros/adi.hpp"
 #include "voss/localizer/AbstractLocalizer.hpp"
-#include <memory>
 #include <atomic>
-
+#include <memory>
 
 namespace voss::localizer {
 
 class ADILocalizer : public AbstractLocalizer {
 
 private:
-	std::atomic<double> prev_left_pos, prev_right_pos, prev_middle_pos;
-	std::atomic<Pose> prev_pose;
+  double prev_left_pos = {0.0}, prev_right_pos = {0.0}, prev_middle_pos = {0.0};
+  Pose prev_pose = {0.0, 0.0, 0.0};
 
-	std::atomic<double> left_right_tpi, middle_tpi;
-	std::atomic<double> track_width;
-	std::atomic<double> left_right_dist, middle_dist;
+  double left_right_tpi = {0.0}, middle_tpi = {0.0};
+  double track_width = {0.0};
+  double left_right_dist = {0.0}, middle_dist = {0.0};
 
-	std::unique_ptr<pros::adi::Encoder> left_encoder;
-	std::unique_ptr<pros::adi::Encoder> right_encoder;
-	std::unique_ptr<pros::adi::Encoder> middle_encoder;
+  std::unique_ptr<pros::adi::Encoder> left_encoder;
+  std::unique_ptr<pros::adi::Encoder> right_encoder;
+  std::unique_ptr<pros::adi::Encoder> middle_encoder;
 
 public:
-	ADILocalizer(int left, int right, int mid, double lr_tpi, double mid_tpi,
-	             double track_width, double middle_dist);
+  ADILocalizer(int left, int right, int mid, double lr_tpi, double mid_tpi,
+               double track_width, double middle_dist);
 
-	int getLeftEncoderValue();
-	int getRightEncoderValue();
-	int getMiddleEncoderValue();
+  int getLeftEncoderValue();
+  int getRightEncoderValue();
+  int getMiddleEncoderValue();
 
-	void update();
+  void update();
 };
 
 } // namespace voss::localizer
