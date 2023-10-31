05ae20767f048bb54c95d1d199686864e4765e15
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-10-31 19:36:10 -0400

    Add X Drive Chassis

---
+++ include/VOSS/chassis/XDriveChassis.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "AbstractChassis.hpp"
+#include "ChassisCommand.hpp"
+#include "pros/motors.hpp"
+#include <memory>
+
+namespace voss::chassis {
+
+class XDriveChassis : public AbstractChassis {
+
+private:
+	std::unique_ptr<pros::Motor> front_left_motor;
+	std::unique_ptr<pros::Motor> back_left_motor;
+	std::unique_ptr<pros::Motor> front_right_motor;
+	std::unique_ptr<pros::Motor> back_right_motor;
+
+public:
+	XDriveChassis(int8_t front_left_motor,
+								int8_t back_left_motor,
+								int8_t front_right_motor,
+								int8_t back_right_motor,
+								controller::AbstractController& default_controller);
+
+	void holonomic(double forward_speed, double turn_speed, double strafe_speed);
+	void tank(double left_speed, double right_speed);
+	void arcade(double forward_speed, double turn_speed);
+};
+
+}
---
+++ src/VOSS/chassis/XDriveChassis.cpp
@@ -0,0 +1,32 @@
+#include "XDriveChassis.hpp"
+
+voss::chassis::XDriveChassis::XDriveChassis(
+		int8_t front_left_motor, int8_t back_left_motor, int8_t front_right_motor,
+		int8_t back_right_motor,
+		controller::AbstractController &default_controller) : AbstractChassis(default_controller) {
+	this->front_left_motor = std::make_unique<pros::Motor>(front_left_motor);
+	this->back_left_motor = std::make_unique<pros::Motor>(back_left_motor);
+	this->front_right_motor = std::make_unique<pros::Motor>(front_right_motor);
+	this->back_right_motor = std::make_unique<pros::Motor>(back_right_motor);
+}
+
+void voss::chassis::XDriveChassis::holonomic(double forward_speed, double turn_speed, double strafe_speed) {
+	this->front_left_motor->move_voltage(forward_speed + turn_speed + strafe_speed);
+	this->back_left_motor->move_voltage(forward_speed + turn_speed - strafe_speed);
+	this->front_right_motor->move_voltage(forward_speed - turn_speed - strafe_speed);
+	this->back_right_motor->move_voltage(forward_speed - turn_speed + strafe_speed);
+}
+
+void voss::chassis::XDriveChassis::tank(double left_speed, double right_speed) {
+	this->front_left_motor->move_voltage(left_speed);
+	this->back_left_motor->move_voltage(left_speed);
+	this->front_right_motor->move_voltage(right_speed);
+	this->back_right_motor->move_voltage(right_speed);
+}
+
+void voss::chassis::XDriveChassis::arcade(double forward_speed, double turn_speed) {
+	this->front_left_motor->move_voltage(forward_speed + turn_speed);
+	this->back_left_motor->move_voltage(forward_speed + turn_speed);
+	this->front_right_motor->move_voltage(forward_speed - turn_speed);
+	this->back_right_motor->move_voltage(forward_speed - turn_speed);
+}
