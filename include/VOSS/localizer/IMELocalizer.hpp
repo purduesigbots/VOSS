2f2f161f80233cc620b320c6ec9d0fc7b71cbae0
Mihir Laud <mihirlaud@gmail.com>
2023-12-05 19:14:34 -0500

    Change all IMELocalizer class members to atomic

--- include/VOSS/localizer/IMELocalizer.hpp
+++ include/VOSS/localizer/IMELocalizer.hpp
@@ -4,6 +4,7 @@
 #include "pros/imu.hpp"
 #include "pros/motor_group.hpp"
 #include "voss/localizer/AbstractLocalizer.hpp"
+#include <atomic>
 #include <memory>
 
 namespace voss::localizer {
@@ -11,13 +12,13 @@ namespace voss::localizer {
 class IMELocalizer : public AbstractLocalizer {
 
 private:
-	double prev_left_pos, prev_right_pos, prev_middle_pos;
-	Pose prev_pose;
+	std::atomic<double> prev_left_pos, prev_right_pos, prev_middle_pos;
+	AtomicPose prev_pose;
 
-	double left_right_tpi, middle_tpi;
-	double track_width;
-	double left_right_dist, middle_dist;
-	int imu_ports;
+	std::atomic<double> left_right_tpi, middle_tpi;
+	std::atomic<double> track_width;
+	std::atomic<double> left_right_dist, middle_dist;
+	std::atomic<int> imu_ports;
 
 	std::unique_ptr<pros::MotorGroup> left_motors;
 	std::unique_ptr<pros::MotorGroup> right_motors;
