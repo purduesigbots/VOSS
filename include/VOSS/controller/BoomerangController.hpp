024687435e48085d12a316bb3c39e727ea684ec0
Rocky14683 <rocky10208@gmail.com>
2024-02-08 14:48:35 -0500

    Implement chained boomerang

--- include/VOSS/controller/BoomerangController.hpp
+++ include/VOSS/controller/BoomerangController.hpp
@@ -9,15 +9,34 @@ namespace voss::controller {
 
 class BoomerangController : public AbstractController {
   protected:
-    std::shared_ptr<PIDController> child = nullptr;
     double lead_pct;
+    Pose carrotPoint;
 
+    double linear_kP, linear_kI, linear_kD;
+    double angular_kP, angular_kI, angular_kD;
+    double vel;
+    double exit_error;
+    double angular_exit_error;
+    double min_error;
+    bool can_reverse;
+    double settle_time;
+
+    double close;
+    double close_2;
+    int counter;
+    double prev_angle;
+    double min_vel;
+
+    double prev_lin_err, total_lin_err, prev_ang_err, total_ang_err;
   public:
     BoomerangController(std::shared_ptr<localizer::AbstractLocalizer> l);
 
     chassis::ChassisCommand get_command(bool reverse, bool thru) override;
     chassis::ChassisCommand get_angular_command(bool reverse,
                                                 bool thru) override;
+
+    double linear_pid(double error);
+    double angular_pid(double error);
 
     void reset();
 
--- src/VOSS/controller/BoomerangController.cpp
+++ src/VOSS/controller/BoomerangController.cpp
@@ -8,39 +8,155 @@ namespace voss::controller {
 BoomerangController::BoomerangController(
     std::shared_ptr<localizer::AbstractLocalizer> l)
     : AbstractController(l) {
-    child = std::make_shared<PIDController>(l);
 }
 
 chassis::ChassisCommand BoomerangController::get_command(bool reverse,
                                                          bool thru) {
     // TODO: finish
-    if (target.theta > 360) {
-        child->set_target(target, false);
-    } else {
-        Point current_pos = this->l->get_position();
-        double dx = target.x - current_pos.x;
-        double dy = target.y - current_pos.y;
-        double distance_error = sqrt(dx * dx + dy * dy);
-        double at = voss::to_radians(target.theta);
-        Pose carrotPoint = {target.x - distance_error * cos(at) * lead_pct,
-                            target.y - distance_error * sin(at) * lead_pct,
-                            target.theta};
-        // printf("current: %f %f\n", current_pos.x, current_pos.y);
-        // printf("carrotPoint: %f %f\n", carrotPoint.x, carrotPoint.y);
-        child->set_target(carrotPoint, false);
+
+    Point current_pos = this->l->get_position();
+    double dx = target.x - current_pos.x;
+    double dy = target.y - current_pos.y;
+    double distance_error = sqrt(dx * dx + dy * dy);
+    double at = voss::to_radians(target.theta);
+    this->carrotPoint = {target.x - distance_error * cos(at) * lead_pct,
+                         target.y - distance_error * sin(at) * lead_pct,
+                         target.theta};
+    counter += 10;
+    int dir = reverse ? -1 : 1;
+
+    double current_angle = this->l->get_orientation_rad();
+    bool chainedExecutable = false;
+    bool noPose = this->target.theta == 361;
+    double angle_error;
+    //    double adv_dist = fabs(this->vel) * 0.001;
+    bool slowDown = false;
+    double m = fabs((current_pos.y - target.y) / (current_pos.x - target.x));
+
+    angle_error = atan2(dy, dx) - current_angle;
+
+    if (reverse) {
+        angle_error = atan2(-dy, -dx) - current_angle;
     }
-    return child->get_command(reverse, thru);
+
+    angle_error = voss::norm_delta(angle_error);
+
+    double lin_speed;
+
+    if (thru &&
+        current_pos.y + this->min_error >=
+            (-1.0 / m) * (current_pos.x + min_error - target.x) + target.y) {
+        chainedExecutable = true;
+    }
+
+    if (distance_error <= exit_error ||
+        (distance_error < min_error && fabs(cos(angle_error)) <= 0.1)) {
+        total_lin_err = 0;
+        close += 10;
+    } else {
+        close = 0;
+    }
+
+    if (close > settle_time) {
+        return chassis::ChassisCommand{chassis::Stop{}};
+    }
+
+    if (fabs(distance_error - prev_lin_err) < 0.1 &&
+        fabs(current_angle - prev_angle) < voss::to_radians(0.1) &&
+        counter > 400) {
+        close_2 += 10;
+    } else {
+        close_2 = 0;
+    }
+
+    prev_angle = current_angle;
+
+    if (close_2 > settle_time * 2) {
+        return chassis::ChassisCommand{chassis::Stop{}};
+    }
+
+    lin_speed = dir * linear_pid(distance_error);
+    if(thru){
+        lin_speed = fmax(lin_speed, this->min_vel);
+    }
+
+
+    double ang_speed;
+    if (distance_error < min_error && !thru) {
+        this->can_reverse = true;
+
+        if (noPose) {
+            ang_speed = 0; // disable turning when close to the point to prevent
+                           // spinning
+        } else {
+            // turn to face the finale pose angle if executing a pose movement
+            double poseError = (target.theta * M_PI / 180) - current_angle;
+            while (fabs(poseError) > M_PI)
+                poseError -= 2 * M_PI * poseError / fabs(poseError);
+            ang_speed = angular_pid(poseError);
+        }
+
+        // reduce the linear speed if the bot is tangent to the target
+        lin_speed *= cos(angle_error);
+
+    } else {
+        if (fabs(angle_error) > M_PI_2 && this->can_reverse) {
+            angle_error =
+                angle_error - (angle_error / fabs(angle_error)) * M_PI;
+            lin_speed = -lin_speed;
+        }
+
+        ang_speed = angular_pid(angle_error);
+    }
+
+
+    if (chainedExecutable) {
+        return chassis::ChassisCommand{chassis::Chained{
+            dir * std::fmax(lin_speed, this->min_vel) - ang_speed,
+            dir * std::fmax(lin_speed, this->min_vel) + ang_speed}};
+    }
+
+    return chassis::ChassisCommand{
+        chassis::Voltages{lin_speed - ang_speed, lin_speed + ang_speed}};
 }
 
 chassis::ChassisCommand BoomerangController::get_angular_command(bool reverse,
                                                                  bool thru) {
-    child->set_target(target, false);
-    child->set_angular_target(angular_target, false);
-    return child->get_angular_command(reverse, thru);
+    return chassis::ChassisCommand{chassis::Stop{}};
+}
+
+double BoomerangController::linear_pid(double error) {
+    total_lin_err += error;
+
+    this->vel = error - prev_ang_err;
+
+    double speed = linear_kP * error + linear_kD * (error - prev_lin_err) +
+                   linear_kI * total_lin_err;
+
+    this->prev_lin_err = error;
+
+    return speed;
+}
+
+double BoomerangController::angular_pid(double error) {
+    total_ang_err += error;
+
+    double speed = angular_kP * error + angular_kD * (error - prev_ang_err) +
+                   angular_kI * total_ang_err;
+
+    this->prev_ang_err = error;
+
+    return speed;
 }
 
 void BoomerangController::reset() {
-    child->reset();
+    this->prev_lin_err = 0;
+    this->total_lin_err = 0;
+    this->prev_ang_err = 0;
+    this->total_ang_err = 0;
+    this->can_reverse = false;
+    this->counter = 0;
+    this->min_vel = 0;
 }
 
 } // namespace voss::controller
