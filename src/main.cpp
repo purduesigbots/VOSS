699658afc5b3d00a4bd201fbf0600287f100b294
Mihir Laud <mihirlaud@gmail.com>
2024-02-16 22:38:12 -0500

    Change parameters

--- src/main.cpp
+++ src/main.cpp
@@ -76,40 +76,27 @@ void opcontrol() {
     pros::Controller master(pros::E_CONTROLLER_MASTER);
 
     auto odom = voss::localizer::IMELocalizerBuilder::new_builder()
-                    .with_left_motors({-2, -3, -6, -5})
-                    .with_right_motors({11, 12, 19, 20})
-                    .with_left_right_tpi(18.24) // 19.5
-                    .with_track_width(9.75)     // 3.558
-                    .with_imu(13)
+                    .with_left_motors({-1, 2, -5, 11})
+                    .with_right_motors({-7, 8, -9, 10})
+                    .with_track_width(9.75)
+                    .with_left_right_tpi(44)
+                    .with_imu(20)
                     .build();
 
     odom->begin_localization();
 
     auto pid = voss::controller::PIDControllerBuilder::new_builder(odom)
-                   .with_linear_constants(7, 0.02, 40)
-                   .with_angular_constants(170, 0, 700)
-                   .with_exit_error(1.0)
-                   .with_angular_exit_error(1.0)
+                   .with_linear_constants(5, 0, 12)
+                   .with_angular_constants(2, 0, 20)
+                   .with_tracking_kp(60)
+                   .with_exit_error(3)
+                   .with_angular_exit_error(1)
                    .with_min_error(5)
-                   .with_settle_time(200)
-                   .build();
-    auto arc = voss::controller::ArcPIDControllerBuilder::new_builder(odom)
-                   .with_linear_constants(6, 0, 50)
-                   .with_track_width(14)
-                   .with_exit_error(1.0)
-                   .with_min_error(5.0)
-                   .with_settle_time(200)
-                   .with_slew(8)
+                   .with_settle_time(250)
                    .build();
 
-    auto swing = voss::controller::SwingControllerBuilder::new_builder(odom)
-                     .with_angular_constants(170, 0, 700)
-                     .with_angular_exit_error(0.5)
-                     .with_settle_time(200)
-                     .build();
-
-    voss::chassis::DiffChassis chassis({-2, -3, -6, -5}, {11, 12, 19, 20}, pid,
-                                       8);
+    auto chassis =
+        voss::chassis::DiffChassis({-1, 2, -5, 11}, {-7, 8, -9, 10}, pid, 8);
 
     auto [leftM, rightM] = chassis.getMotors();
 
