ff8e76fb61276dca03b220ce8d7130e91c956274
THERocky <101498190+Rocky14683@users.noreply.github.com>
2024-04-16 14:28:31 -0400

    fix typo (#94)

--- src/VOSS/exit_conditions/ToleranceExitCondition.cpp
+++ src/VOSS/exit_conditions/ToleranceExitCondition.cpp
@@ -5,7 +5,7 @@ namespace voss::controller {
 
 bool ToleranceExitCondition::is_met(Pose pose, bool thru) {
     if (this->target_has_coordinate() && this->target_has_heading()) {
-        return ang_exit->is_met(pose, thru) && ang_exit->is_met(pose, thru);
+        return ang_exit->is_met(pose, thru) && lin_exit->is_met(pose, thru);
     } else if (this->target_has_coordinate()) {
         return lin_exit->is_met(pose, thru);
     } else if (this->target_has_heading()) {
