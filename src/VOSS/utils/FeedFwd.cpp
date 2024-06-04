7fa7e992299be616ee418861e89c99301fe53463
Andrew Lu <lu987@purdue.edu>
2024-06-04 12:12:47 -0700

    Add ffwd controller (#103)
    Co-authored-by: Rocky14683 <rocky10208@gmail.com>

---
+++ src/VOSS/utils/FeedFwd.cpp
@@ -0,0 +1,22 @@
+
+#include "VOSS/utils/FeedFwd.hpp"
+#include "VOSS/utils/Math.hpp"
+
+namespace voss {
+
+FeedForward::FeedForward(double kS, double kV, double kA, double kD)
+    : kS(kS), kD(kD), kA(kA), kV(kV){};
+
+double FeedForward::update(double velocity, double acceleration) {
+    return kS * sgn(velocity) + kV * velocity +
+           (acceleration > 0 ? kA : kD) * acceleration;
+}
+
+void FeedForward::set_constants(double kS, double kV, double kA, double kD) {
+    this->kS = kS;
+    this->kV = kV;
+    this->kA = kA;
+    this->kD = kD;
+}
+
+} // namespace voss
