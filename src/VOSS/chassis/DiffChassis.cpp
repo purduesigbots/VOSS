d960b35b25b78f15103bbd592bf8ef5c75f03405
THERocky <101498190+Rocky14683@users.noreply.github.com>
2024-06-28 11:00:53 +0800

    set brake mode bug fix (#106)
    * change set_brake_mode to set_brake_mode_all

--- src/VOSS/chassis/DiffChassis.cpp
+++ src/VOSS/chassis/DiffChassis.cpp
@@ -35,8 +35,8 @@ DiffChassis::DiffChassis(std::initializer_list<int8_t> left_motors,
 
     this->slew_step = slew_step > 0 ? slew_step : 200;
     this->brakeMode = brakeMode;
-    this->left_motors->set_brake_mode(this->brakeMode);
-    this->right_motors->set_brake_mode(this->brakeMode);
+    this->left_motors->set_brake_mode_all(this->brakeMode);
+    this->right_motors->set_brake_mode_all(this->brakeMode);
     this->prev_voltages = {0, 0};
 }
 
@@ -55,88 +55,89 @@ void DiffChassis::arcade(double forward_speed, double turn_speed) {
 
 void DiffChassis::set_brake_mode(pros::motor_brake_mode_e mode) {
     this->brakeMode = mode;
-    this->left_motors->set_brake_mode(mode);
-    this->right_motors->set_brake_mode(mode);
+    this->left_motors->set_brake_mode_all(mode);
+    this->right_motors->set_brake_mode_all(mode);
 }
 
 // Evoke the chassis to move according to how it was set up using the
 // constructor, returns true if movement is complete
 bool DiffChassis::execute(DiffChassisCommand cmd, double max) {
     return std::visit(
-        overload{
-            [this](Stop&) -> bool {
-                this->set_brake_mode(this->brakeMode);
-                this->left_motors->brake();
-                this->right_motors->brake();
+        overload{[this](Stop&) -> bool {
+                     this->set_brake_mode(this->brakeMode);
+                     this->left_motors->brake();
+                     this->right_motors->brake();
 
-                return true;
-            },
-            [this, max](diff_commands::Voltages& v) -> bool {
-                double v_max = std::max(fabs(v.left), fabs(v.right));
-                if (v_max > max) {
-                    v.left = v.left * max / v_max;
-                    v.right = v.right * max / v_max;
-                }
+                     return true;
+                 },
+                 [this, max](diff_commands::Voltages& v) -> bool {
+                     double v_max = std::max(fabs(v.left), fabs(v.right));
+                     if (v_max > max) {
+                         v.left = v.left * max / v_max;
+                         v.right = v.right * max / v_max;
+                     }
 
-                v.left = slew(v.left, true);
-                v.right = slew(v.right, false);
+                     v.left = slew(v.left, true);
+                     v.right = slew(v.right, false);
 
-                this->left_motors->move_voltage(120 * v.left);
-                this->right_motors->move_voltage(120 * v.right);
+                     this->left_motors->move_voltage(120 * v.left);
+                     this->right_motors->move_voltage(120 * v.right);
 
-                this->prev_voltages = v;
+                     this->prev_voltages = v;
 
-                return false;
-            },
-            // Logic allowing for individual movements within a chain of
-            // movements to be registered at completed even though robot may
-            // still be moving
-            [this, max](diff_commands::Chained& v) -> bool {
-                double v_max = std::max(fabs(v.left), fabs(v.right));
-                if (v_max > max) {
-                    v.left = v.left * max / v_max;
-                    v.right = v.right * max / v_max;
-                }
+                     return false;
+                 },
+                 // Logic allowing for individual movements within a chain of
+                 // movements to be registered at completed even though robot
+                 // may still be moving
+                 [this, max](diff_commands::Chained& v) -> bool {
+                     double v_max = std::max(fabs(v.left), fabs(v.right));
+                     if (v_max > max) {
+                         v.left = v.left * max / v_max;
+                         v.right = v.right * max / v_max;
+                     }
 
-                v.left = slew(v.left, true);
-                v.right = slew(v.right, false);
+                     v.left = slew(v.left, true);
+                     v.right = slew(v.right, false);
 
-                this->left_motors->move_voltage(120 * v.left);
-                this->right_motors->move_voltage(120 * v.right);
+                     this->left_motors->move_voltage(120 * v.left);
+                     this->right_motors->move_voltage(120 * v.right);
 
-                this->prev_voltages = {v.left, v.right};
+                     this->prev_voltages = {v.left, v.right};
 
-                return true;
-            },
-            // Logic to brake one side of the drive alloing for a turn around
-            // the side of the robot and returning true when the turn is
-            // finished
-            [this, max](diff_commands::Swing& v) {
-                double v_max = std::max(fabs(v.left), fabs(v.right));
-                if (v.right == 0) {
-                    this->right_motors->set_brake_mode(pros::MotorBrake::hold);
-                    this->right_motors->brake();
-                    if (v_max > max) {
-                        v.left = v.left * max / v_max;
-                    }
-                    v.left = slew(v.left, true);
-                    this->left_motors->move_voltage(120 * v.left);
-                    this->prev_voltages = {v.left, 0.0};
+                     return true;
+                 },
+                 // Logic to brake one side of the drive alloing for a turn
+                 // around the side of the robot and returning true when the
+                 // turn is finished
+                 [this, max](diff_commands::Swing& v) {
+                     double v_max = std::max(fabs(v.left), fabs(v.right));
+                     if (v.right == 0) {
+                         this->right_motors->set_brake_mode_all(
+                             pros::MotorBrake::hold);
+                         this->right_motors->brake();
+                         if (v_max > max) {
+                             v.left = v.left * max / v_max;
+                         }
+                         v.left = slew(v.left, true);
+                         this->left_motors->move_voltage(120 * v.left);
+                         this->prev_voltages = {v.left, 0.0};
 
-                } else if (v.left == 0) {
-                    this->left_motors->set_brake_mode(pros::MotorBrake::hold);
-                    this->left_motors->brake();
-                    if (v_max > max) {
-                        v.right = v.right * max / v_max;
-                    }
-                    v.right = slew(v.right, false);
+                     } else if (v.left == 0) {
+                         this->left_motors->set_brake_mode_all(
+                             pros::MotorBrake::hold);
+                         this->left_motors->brake();
+                         if (v_max > max) {
+                             v.right = v.right * max / v_max;
+                         }
+                         v.right = slew(v.right, false);
 
-                    this->right_motors->move_voltage(120 * v.right);
-                    this->prev_voltages = {0.0, v.right};
-                }
+                         this->right_motors->move_voltage(120 * v.right);
+                         this->prev_voltages = {0.0, v.right};
+                     }
 
-                return false;
-            }},
+                     return false;
+                 }},
         cmd);
 }
 
