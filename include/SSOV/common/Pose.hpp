64a2cce6a20eacc84d41da2c4a4d566f9b042fbc
Andrew Lu <lu987@purdue.edu>
2025-01-27 15:09:33 -0500

    pose math

--- include/SSOV/common/Pose.hpp
+++ include/SSOV/common/Pose.hpp
@@ -6,18 +6,55 @@ namespace ssov {
     struct Point {
         double x;
         double y;
+
         Point(double x, double y): x(x), y(y) {};
         Point(): Point(0, 0) {};
+
+        Point operator+(const Point &other) const {
+            return {x + other.x, y + other.y};
+        }
+        Point operator-(const Point &other) const {
+            return {x - other.x, y - other.y};
+        }
+        Point rotate(double radians) const {
+            double c = cos(radians);
+            double s = sin(radians);
+            return {x * c - y * s, x * s + y * c};
+        }
     };
 
     struct Pose {
         double x;
         double y;
         double theta;
+
         Pose(double x, double y, double theta): x(x), y(y), theta(theta) {};
         Pose(): Pose(0, 0, 0) {};
         Point to_point() const {
             return {x, y};
+        }
+
+        Pose rotate(double radians) const {
+            double c = cos(radians);
+            double s = sin(radians);
+            return {x * c - y * s, x * s + y * c, theta + radians};
+        }
+        Pose operator+(const Pose &other) const {
+            Pose rotated = other.rotate(theta);
+            return {x + rotated.x, y + rotated.y, rotated.theta};
+        }
+        Pose operator-(const Pose &other) const {
+            double angle = theta - other.theta;
+            Point point = to_point() - other.to_point().rotate(angle);
+            return {point.x, point.y, angle};
+        }
+        Pose operator+(const Point &other) const {
+            Point rotated = other.rotate(theta);
+            return {x + rotated.x, y + rotated.y, theta};
+        }
+        Pose operator-(const Point &other) const {
+            Point rotated = other.rotate(theta);
+            return {x - rotated.x, y - rotated.y, theta};
         }
     };
 
