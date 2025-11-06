0c42757783b5dfa13e39c15343bdbab93e88b6bb
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2025-11-05 19:25:48 -0500

    Fix standard versions

--- common.mk
+++ common.mk
@@ -20,8 +20,8 @@ WARNFLAGS+=-Wno-psabi
 SPACE := $() $()
 COMMA := ,
 
-C_STANDARD?=gnu11
-CXX_STANDARD?=gnu++20
+C_STANDARD?=gnu2x
+CXX_STANDARD?=gnu++23
 
 DEPDIR := .d
 $(shell mkdir -p $(DEPDIR))
