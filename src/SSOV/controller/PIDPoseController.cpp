bbfa6ef8ccad958723db8678e95cfd812cafd4bd
QuadBlueShadow <99780232+QuadBlueShadow@users.noreply.github.com>
2025-11-10 20:28:33 -0500

    Added some comments

--- src/SSOV/controller/PIDPoseController.cpp
+++ src/SSOV/controller/PIDPoseController.cpp
@@ -27,28 +27,30 @@ DriveSignal PIDPoseController::compute(const Pose &current_pose, const Pose &tar
         angle_error = atan2(-dy, -dx) - current_pose.theta;
     }
 
+    //We don't want things to blow up
     if (angle_error == NAN){
         angle_error = 0;
     }
-    //printf("Angle Error: %f\n", strafe_angle);
 
-    //printf("Angle Error: %f\n", angle_error);
-
+    //If we are on a holonomic drive we have different logic
     if (holonomic){
+        //We split our speed into linear and horizontal speeds for our drive train
         double direct_speed = (thru ? 100.0 : (linear_pid.update(distance_error))) * dir;
-        lin_speed = direct_speed * cos(angle_error); //* (std::abs(angle_error) > 1 ? -1 : 1);
+        lin_speed = direct_speed * cos(angle_error);
         hor_speed = direct_speed * sin(angle_error) * sideways_multiplier;
-
-        //printf("%.2f, %.2f\n", lin_speed, hor_speed);
     }
     else {
         lin_speed = (thru ? 100.0 : (linear_pid.update(distance_error))) * dir;
     }
 
+    //We want the ability to strafe
+    //As long as our strafe angle is within [-2p, 2pi] we actually strafe
+    //To not use a strafe angle just set the strafe angle higher than 2pi or lower than -2pi
     if(std::abs(strafe_angle) <= 2 * M_PI){
         angle_error = strafe_angle - current_pose.theta;
     }
 
+    //Norm delta the angle
     angle_error = norm_delta(angle_error);
     
     double ang_speed;
@@ -67,6 +69,9 @@ DriveSignal PIDPoseController::compute(const Pose &current_pose, const Pose &tar
         ang_speed = angular_pid.update(min_dist_ang_err);
         //printf("Angular speed norm: %0.2f, Angle: %0.2f, Desired Angle: %0.2f\n", ang_speed, current_pose.theta, target_point.theta);
     } else {
+        //FIX THIS OR YOU DIE YOU GOOBERS IT WILL NOT LOSE MORE SLEEP
+        //AHGHGHGHGHGHGHHGHHHHHHHHHHH
+        //If we are close enough to our target we want to turn to our target pose
         if (distance_error < min_error * final_angle_multiplier)
             angle_error = norm_delta(target_point.theta - current_pose.theta);
             
@@ -80,6 +85,7 @@ DriveSignal PIDPoseController::compute(const Pose &current_pose, const Pose &tar
         //printf("  Angular speed: %f\n", ang_speed);
     }
 
+    //Clamp our stuff
     lin_speed = std::clamp(lin_speed, -100.0, 100.0);
     hor_speed = std::clamp(hor_speed, -100.0, 100.0);
     if (debug) {
